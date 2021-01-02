16b60b99d5e54169692b6333815be21ab58f7a3d
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2021-01-02 20:32:34 +0100

    create landing page

---
+++ app.js
@@ -0,0 +1,24 @@
+const nav = document.querySelector('#navigation');
+const navLink = document.querySelectorAll('.nav__link');
+const toggleNav = document.querySelector('#toggle-nav');
+const closeNav = document.querySelector('#close-nav');
+
+// open & close nav
+
+toggleNav.addEventListener('click', () => {
+    nav.classList.add('show');
+});
+
+closeNav.addEventListener('click', () => {
+    nav.classList.remove('show');
+});
+
+// active links
+
+function linkAction() {
+    navLink.forEach((link) => link.classList.remove('active'));
+    this.classList.add('active');
+    nav.classList.remove('show');
+}
+
+navLink.forEach((link) => link.addEventListener('click', linkAction));
