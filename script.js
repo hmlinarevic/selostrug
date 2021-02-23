fdac195a6ebd08340fe1e96502b118589c55964b
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2021-02-23 18:47:42 +0100

    add section content

--- script.js
+++ script.js
@@ -3,27 +3,27 @@ const btnNavClose = document.querySelector('#nav-close');
 const navContent = document.querySelector('.nav__menu');
 
 class App {
-    constructor() {
-        btnNavToggle.addEventListener('click', this.toggleNavMenu);
-        btnNavClose.addEventListener('click', this.closeNavMenu);
-        this.sayHi();
-    }
+  constructor() {
+    btnNavToggle.addEventListener('click', this.toggleNavMenu);
+    btnNavClose.addEventListener('click', this.closeNavMenu);
+    this.sayHi();
+  }
 
-    toggleNavMenu() {
-        navContent.classList.toggle('nav__menu--show');
-        // prevent scroll
-        document.body.classList.add('stop-scrolling');
-    }
+  toggleNavMenu() {
+    navContent.classList.toggle('nav__menu--show');
+    // prevent scroll
+    document.body.classList.add('stop-scrolling');
+  }
 
-    closeNavMenu() {
-        navContent.classList.remove('nav__menu--show');
-        // allow scroll
-        document.body.classList.remove('stop-scrolling');
-    }
+  closeNavMenu() {
+    navContent.classList.remove('nav__menu--show');
+    // allow scroll
+    document.body.classList.remove('stop-scrolling');
+  }
 
-    sayHi() {
-        console.log('hi there');
-    }
+  sayHi() {
+    console.log('hi there');
+  }
 }
 
 new App();
