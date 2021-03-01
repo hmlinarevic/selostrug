98fb758e4b310e0a331ef9142d3f91133271c154
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2021-03-01 17:14:13 +0100

    add mouse over/out effect on nav links

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const btnNavToggle = document.querySelector('#nav-toggle');
 const btnNavClose = document.querySelector('#nav-close');
 const navContent = document.querySelector('.nav__menu');
+const header = document.querySelector('.header');
+const nav = document.querySelector('.nav');
+
+console.log(nav);
 
 class App {
   constructor() {
     btnNavToggle.addEventListener('click', this.toggleNavMenu);
     btnNavClose.addEventListener('click', this.closeNavMenu);
+    nav.addEventListener('mouseover', this.handleHover.bind(0.5));
+    nav.addEventListener('mouseout', this.handleHover.bind(1));
+
     this.sayHi();
   }
 
@@ -19,6 +26,19 @@ class App {
     navContent.classList.remove('nav__menu--show');
     // allow scroll
     document.body.classList.remove('stop-scrolling');
+  }
+
+  handleHover(e) {
+    if (e.target.classList.contains('nav__link')) {
+      const link = e.target;
+      const siblings = link.closest('nav').querySelectorAll('.nav__link');
+
+      siblings.forEach((el) => {
+        if (el !== link) {
+          el.style.opacity = this;
+        }
+      });
+    }
   }
 
   sayHi() {
